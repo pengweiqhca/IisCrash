2f7c595d64756fb7e469b7d8fb1fe02ceeb63eec
彭伟 <pengwei@tuhu.cn>
2018-05-29 13:46:47 +0800

    first commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+namespace IisCrash.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index() => new JsonResult { Data = Request.RequestContext.RouteData, JsonRequestBehavior = JsonRequestBehavior.AllowGet };
+    }
+}
---
+++ Global.asax.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace IisCrash
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            RegisterRoutes(RouteTable.Routes);
+        }
+
+        private static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+        }
+    }
+}
